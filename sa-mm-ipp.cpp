dfefab05dab6692b6897ded9875f0115e504c990
James Thomas <jamesjoethomas@gmail.com>
2017-03-31 20:02:24 -0700

    ipp radix sort-based sa

--- sa-mm-ipp.cpp
+++ sa-mm-ipp.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <vector>
 #include <limits.h>
+#include <assert.h>
+#include <ipps_l.h>
 
 static inline uint64_t
 rdtsc(void)
@@ -27,30 +29,12 @@ rdtsc(void)
 
 using namespace std;
 
-typedef struct {
-  int first;
-  int second;
-  int i;
+typedef struct __attribute__((packed)) {
+  unsigned int: 1;
+  unsigned int i: 21;
+  unsigned int second: 21;
+  unsigned int first: 21;
 } p;
-
-int compare(const void *a, const void *b) {
-  p *a_p = (p *)a;
-  p *b_p = (p *)b;
-  if (a_p->first != b_p->first) {
-    return a_p->first - b_p->first;
-  } else {
-    return a_p->second - b_p->second;
-  }
-};
-
-bool verify_sorted(p *data, int size) {
-  for (int i = 1; i < size; i++) {
-    if (compare(data + i, data + i - 1) < 0) {
-      return false;
-    }
-  }
-  return true;
-}
 
 int main(int argc, char **argv) {
   int CHARS = atoi(argv[1]);
@@ -68,6 +52,23 @@ int main(int argc, char **argv) {
     chars += line.length();
   }
 
+  p test[0];
+  uint64_t *u = (uint64_t *)test;
+  test[0].i = 0;
+  test[0].second = 0;
+  test[0].first = 1;
+  uint64_t f_test = *u;
+  test[0].i = 0;
+  test[0].second = 1;
+  test[0].first = 0;
+  uint64_t s_test = *u;
+  test[0].i = 1;
+  test[0].second = 0;
+  test[0].first = 0;
+  uint64_t i_test = *u;
+  assert(f_test > s_test);
+  assert(s_test > i_test);
+
   p *data1 = (p *)malloc(sizeof(p) * chars);
   p *data2 = (p *)malloc(sizeof(p) * chars);
   p *data3 = (p *)malloc(sizeof(p) * chars);
@@ -80,57 +81,21 @@ int main(int argc, char **argv) {
   }
   int *counts = (int *)malloc(sizeof(int) * (chars + 1));
   int gap = 2;
- 
+
+  IppSizeL buf_size;
+  ippsSortRadixGetBufferSize_L(chars, ipp64u, &buf_size);
+  Ipp8u *w_buf = (Ipp8u *)malloc(buf_size * sizeof(Ipp8u));
+
   struct timeval start, end, diff;
   gettimeofday(&start, 0);
   uint64_t top_start = rdtsc();
   uint64_t radix_time = 0;
   uint64_t rank_time = 0;
   while (true) {
-    uint64_t radix_start = rdtsc();
-    // radix pass 1
-    for (int i = 0; i < chars + 1; i++) {
-      counts[i] = 0;
-    }
-    for (int i = 0; i < chars; i++) {
-      counts[data1[i].second]++;
-    }
-    int sum = 0;
-    for (int i = 0; i < chars + 1; i++) {
-      int new_sum = sum + counts[i];
-      counts[i] = sum;
-      sum = new_sum;
-    }
-    for (int i = 0; i < chars; i++) {
-      data2[counts[data1[i].second]++] = data1[i];
-    }
-    // radix pass 2
-    for (int i = 0; i < chars + 1; i++) {
-      counts[i] = 0;
-    }
-    for (int i = 0; i < chars; i++) {
-      counts[data2[i].first]++;
-    }
-    sum = 0;
-    for (int i = 0; i < chars + 1; i++) {
-      int new_sum = sum + counts[i];
-      counts[i] = sum;
-      sum = new_sum;
-    }
-    for (int i = 0; i < chars; i++) {
-      data3[counts[data2[i].first]++] = data2[i];
-    }
-    radix_time += (rdtsc() - radix_start);
-    /*
-    // sort
     memcpy(data3, data1, sizeof(p) * chars);
-    qsort(data3, chars, sizeof(p), compare);
-    */
-    /*
-    if (!verify_sorted(data3, chars)) {
-      printf("not sorted at %d\n", gap);
-    }
-    */
+    uint64_t radix_start = rdtsc();
+    ippsSortRadixAscend_64u_I_L((Ipp64u *)data3, chars, w_buf);
+    radix_time += (rdtsc() - radix_start);
 
     uint64_t rank_start = rdtsc();
     bool dups = false;
